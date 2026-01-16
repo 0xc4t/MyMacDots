41f24e3061834ebe96d73fd250fca9fd3df6bef5
0xc4t <105418279+0xc4t@users.noreply.github.com>
2026-01-16 13:37:33 +0700

    Delete home directory

--- home/nvim/lua/user/treesitter.lua
+++
@@ -1,5 +0,0 @@
-require("nvim-treesitter.configs").setup({
-  ensure_installed = { "lua", "javascript", "python", "cpp" },
-  highlight = { enable = true },
-  indent = { enable = true },
-})
--- home/sketchybar/plugins/space.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$SELECTED" = "true" ]; then
-  sketchybar --set "$NAME" \
-    icon.color=0xffcdd6f4
-else
-  sketchybar --set "$NAME" \
-    icon.color=0xffa6adc8
-fi
